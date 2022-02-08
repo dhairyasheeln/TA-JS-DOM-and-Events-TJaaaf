3b513a2b245f902beca9bc193e11c6ef31a8e21b
dhairyasheel <dhairyasheeln@gmail.com>
2022-02-08 17:34:05 +0530

    Random Color Generator - Final

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -14,27 +14,29 @@ let colors=['#99ccff','#ff5050','#ccff99','#ffff66','#cc6699','#996633','#339966
 
 let i=0;
 
+/*Genrate random Color*/
+
+function generateRandom()
+{
+    let hex=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+    let color="#";
+    for(let i=0;i<6;i++)
+    {
+        let random=Math.floor(Math.random()*16);
+        color=color+hex[random];
+    }
+    return color;
+}
 
 /*Event Handler for click event*/
 function changeColorClick()
 {
-    if(i>=colors.length)
-    {
-        i=0;
-    }
-    firstBox.style.backgroundColor=colors[i];
-    i++;
-
+    firstBox.style.backgroundColor=generateRandom();
 }
 
 /*Event Handler for Mouse move event*/
 function changeColorMouse()
 {
-    if(i>=colors.length)
-    {
-        i=0;
-    }
-    secondBox.style.backgroundColor=colors[i];
-    i++;
-
+        secondBox.style.backgroundColor=generateRandom();
 }
